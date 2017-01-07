13eb29fa07dd3c6da21f98682d960630051d11bd
kwoolter <keith.woolterton@gmail.com>
2017-01-07 08:04:13 +0800

    new graphics files

--- yahtzee/view/graphics.py
+++ yahtzee/view/graphics.py
@@ -1,5 +1,23 @@
+import pygame,os, sys
+
+
 class MainFrame:
-    def __init__(self):
+
+    RESOURCES_DIR = os.path.dirname(__file__) + "\\resources\\"
+
+    def __init__(self, name : str, width : int = 800, height : int = 600):
+        self.name = name
+        self.surface = pygame.display.set_mode((width, height))
+
+    def initialise(self):
+        pygame.init()
+        pygame.display.set_caption(self.name)
+        filename = MainFrame.RESOURCES_DIR + self.name + ".jpg"
+        image = pygame.image.load(filename)
+        image = pygame.transform.scale(image, (32, 32))
+        pygame.display.set_icon(image)
+
+
+
+    def draw(self):
         pass
-
-
